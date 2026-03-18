6241cb757467003b0d7536d9372ef8f0aadde70a
thasegawadesign <hello@hasegawatatsuya.com>
2026-03-18 18:12:46 +0900

    feat: implement customer voice carousel logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import EmblaCarousel from "@/components/EmblaCarousel/EmblaCarousel";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <EmblaCarousel slides={[1, 2, 3, 4, 5]} />
+      </main>
+    </>
+  );
 }
